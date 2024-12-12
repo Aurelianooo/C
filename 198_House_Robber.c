f880f8155abe17976bf52a3416838e9b8b8f05ce
Aurelianooo <54444048+Aurelianooo@users.noreply.github.com>
2024-12-12 15:29:32 +0800

    Update 198_House_Robber.c

--- 198_House_Robber.c
+++ 198_House_Robber.c
@@ -7,21 +7,14 @@
 // -lm选项告诉编译器，我们程序中用到的数学函数要到这个库文件里找。
 
 int rob(int* nums, int numsSize) {
-    if(numsSize==1) return nums[0];
-    else if(numsSize==2) return nums[0]>nums[1] ? nums[0]:nums[1];
-    else if(numsSize==3) return nums[0]+nums[2]>nums[1]? nums[0]+nums[2]:nums[1];
-    int* ans=(int*)malloc(numsSize*sizeof(int));
-    int i;
-    for(i=0;i<numsSize;i++) ans[i]=0;
+    int ans[101]={0};
     ans[0]=nums[0];
+    if(numsSize==1) return ans[0];
     ans[1]=nums[0]>nums[1] ? nums[0]:nums[1];
-    ans[2]=nums[0]+nums[2]>nums[1]? nums[0]+nums[2]:nums[1];
-    for(i=3;i<numsSize;i++){
-        int tmp=fmax(ans[i-3]+nums[i-1], ans[i-2]+nums[i]);
-        tmp=fmax(tmp, ans[i-3]+nums[i]);
-        ans[i]=tmp;
+    if(numsSize==2) return ans[1];
+    for(int i=2;i<numsSize;i++){
+        ans[i]=fmax(ans[i-1], ans[i-2]+nums[i]);
     }
-
     return ans[numsSize-1];
 }
 
