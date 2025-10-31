0c4a388ef56701c87daf33791c03df97ad846008
Aureliano <54444048+Aurelianooo@users.noreply.github.com>
2025-10-31 11:01:20 +0800

    Modify insertToTireTree to accept pointer data

--- Tire/main.c
+++ Tire/main.c
@@ -4,27 +4,35 @@
 int main(void)
 {
     struct TireTree *tree = initTireTree(26); // lowercase letters only
-    insertToTireTree(tree, "hello", 2);
-    insertToTireTree(tree, "helloworld", 3);
-    insertToTireTree(tree, "hel", 4);
-    insertToTireTree(tree, "worldhello", 5);
+    int data = 2;
+    insertToTireTree(tree, "hello", (void *)&data, sizeof(int));
+    data = 3;
+    insertToTireTree(tree, "helloworld", (void *)&data, sizeof(int));
+    data = 4;
+    insertToTireTree(tree, "hel", (void *)&data, sizeof(int));
+    data = 5;
+    insertToTireTree(tree, "worldhello", (void *)&data, sizeof(int));
     struct TreeNode *res = NULL;
     char *str = "hello";
     res = findFromTireTree(tree, str, NULL);
     if (res)
+    {
         printf("Tire tree has %s\n", str);
+        printf("Node data is %d\n", *(int *)res->data);
+    }
     removeFromTireTree(tree, str);
     printf("Tire tree has %d words\n", tree->wordCnt);
     res = findFromTireTree(tree, str, NULL);
     if (!res)
         printf("Tire tree has not %s\n", str);
-    str = "he";
+    str = "hel";
     res = findFromTireTree(tree, str, NULL);
     if (res)
         printf("Tire tree has %s\n", str);
     else
         printf("Tire tree has not %s\n", str);
 
+    printf("Tree root childcnt=%d, wordcnt=%d, nodecnt=%d\n", tree->childCnt, tree->wordCnt, tree->nodeCnt);
     destroyTireTree(tree);
     return 0;
 }
