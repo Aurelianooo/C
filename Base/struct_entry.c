b864d186abde62c8e8be12c3bcb8e504cae8b0f1
Aureliano <54444048+Aurelianooo@users.noreply.github.com>
2025-11-07 11:13:06 +0800

    Update struct_entry.c

--- Base/struct_entry.c
+++ Base/struct_entry.c
@@ -5,14 +5,9 @@
     ((type *)((char *)(ptr) - (unsigned long)(&((type *)0)->member)))
 
 #define offsetof(TYPE, MEMBER) ((size_t)&((TYPE *)0)->MEMBER)
-/**Linus edition*
- * container_of - 通过结构体的一个成员获取容器结构体的指针
- * @ptr: 指向成员的指针。
- * @type: 成员所嵌入的容器结构体类型。
- * @member: 结构体中的成员名。
- */
+
 #define container_of(ptr, type, member) ({             \
-    const typeof(((type *)0)->member) *__mptr = (ptr); \
+    const typeof(&((type *)0)->member) __mptr = (ptr); \
     (type *)((char *)__mptr - offsetof(type, member)); \
 })
 
@@ -45,24 +40,24 @@ typedef struct Teacher
 
 int main()
 {
-    /*     Student stu;
-        stu.age = 10;
-        stu.name = "Zhang San";
-        stu.tel = "12345678";
-        stu.gender = MALE;
+    Student stu;
+    stu.age = 10;
+    stu.name = "Zhang San";
+    stu.tel = "12345678";
+    stu.gender = MALE;
 
-        Student *p = &stu;
-        printf("stu address:%p\n", p);
-        char **q = &stu.tel;
-        printf("stu.tel address:%p\n", q);
+    Student *p = &stu;
+    printf("stu address:%p\n", p);
+    char **q = &stu.tel;
+    printf("stu.tel address:%p\n", q);
 
-        p = NULL;
-        // p = struct_entry(q, Student, tel);
-        p = container_of(q, Student, tel);
-        printf("pointer p:%p\n", p);
+    p = NULL;
+    // p = struct_entry(q, Student, tel);
+    p = container_of(q, Student, tel);
+    printf("pointer p:%p\n", p);
 
-        printf("\n");
-     */
+    printf("\n");
+    
     Teacher tea;
     tea.age = 42;
     strcpy(tea.name, "Li Si");
@@ -73,10 +68,10 @@ int main()
     printf("tea address:%p\n", m);
 
     char (*n)[TEL_LEN] = &tea.tel;
-    UNUSED(n);
-    // m = NULL;
-    // m = container_of(n, Teacher, tel);
-    // printf("pointer m:%p\n", m);
+    // UNUSED(n);
+    m = NULL;
+    m = container_of(n, Teacher, tel);
+    printf("pointer m:%p\n", m);
 
     const typeof(&((Teacher *)0)->tel) ptr = &tea.tel;
     printf("pointer ptr:%p\n", ptr);
