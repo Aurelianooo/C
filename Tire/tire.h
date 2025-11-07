d8c871a61f1e774ee1fe718935d8b2a8eea81162
Aureliano <54444048+Aurelianooo@users.noreply.github.com>
2025-11-07 08:18:34 +0800

    Refactor data pointer usage in tire.h

--- Tire/tire.h
+++ Tire/tire.h
@@ -1,13 +1,16 @@
 #ifndef TIRE_TREE_H
 #define TIRE_TREE_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 typedef struct TreeNode
 {
     struct TreeNode **child;
+    void *data;
     size_t size;
-    void* data;
     bool end;
 } TreeNode;
 
@@ -19,10 +22,12 @@ typedef struct TireTree
     int nodeCnt;
 } TireTree;
 
-struct TreeNode *initTreeNode(struct TireTree *tree, void* _data, size_t _size, const bool _end);
+struct TreeNode *initTreeNode(struct TireTree *tree, void *_data, size_t _size, const bool _end);
 struct TireTree *initTireTree(const int _childCnt);
 void destroyTireTree(struct TireTree *tree);
-void insertToTireTree(struct TireTree *tree, const char *s, void* _data, size_t _size);
+void insertToTireTree(struct TireTree *tree, const char *s, void *_data, size_t _size);
+void travelTreeNode(struct TireTree *tree, struct TreeNode *node, void (*callback)(struct TreeNode *, void *), void *arg);
+void travelTireTree(struct TireTree *tree, void (*callback)(struct TreeNode *, void *), void *arg);
 struct TreeNode *findFromTireTree(struct TireTree *tree, const char *s, int *depth);
 void removeFromTireTree(struct TireTree *tree, const char *s);
 
