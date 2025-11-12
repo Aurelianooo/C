8de010a205b76aedbb43c3c3917ed200757b9413
Aureliano <54444048+Aurelianooo@users.noreply.github.com>
2025-11-12 13:05:11 +0800

    Update typeof.c

--- Base/typeof.c
+++ Base/typeof.c
@@ -20,15 +20,21 @@ int main()
 {
     int a = 1;
     int b = 2;
-    SWAP(a, b);
-    printf("a = %d, b = %d\n", a, b);
+    // SWAP(a, b);
+    // printf("a = %d, b = %d\n", a, b);
 
-    int result = MAX(a++, b++); // 安全，x++和y++只执行一次
-    printf("result = %d, x = %d, y = %d\n", result, a, b);
+    // int result = MAX(a++, b++); // 安全，x++和y++只执行一次
+    // printf("result = %d, x = %d, y = %d\n", result, a, b);
+
+    typeof(&a) c = &b;
+    typeof(a) *d = &b;
+    printf("c=%d, d=%d\n", *c, *d);
 
     int arr[] = {1, 2, 3, 4, 5};
-    typeof(arr) *ptr = &arr;
-    printf("ptr[3] = %d\n", (*ptr)[3]);
+    typeof(arr) *ptr1 = &arr;
+    printf("ptr1[3] = %d\n", (*ptr1)[3]);
+    typeof(&arr) ptr2 = &arr;
+    printf("ptr2[2] = %d\n", (*ptr2)[2]);
 
     return 0;
 }
