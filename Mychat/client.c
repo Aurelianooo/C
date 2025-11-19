21162c57346d22a02a8e2937dcc25ab4da2e41b4
Aureliano <54444048+Aurelianooo@users.noreply.github.com>
2025-11-19 09:03:11 +0800

    Update client.c

--- Mychat/client.c
+++ Mychat/client.c
@@ -1,6 +1,6 @@
-
 #include "../wrap/wrap.h"
 
+#define SERV_ADDR "127.0.0.1"
 #define SERV_PORT 8888
 
 int main(int argc, char *argv[])
@@ -12,9 +12,17 @@ int main(int argc, char *argv[])
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8888);
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr.s_addr);
+    inet_pton(AF_INET, SERV_ADDR, &server_addr.sin_addr.s_addr);
 
     Connect(cfd, (struct sockaddr *)&server_addr, sizeof server_addr);
+
+    int flag = fcntl(STDIN_FILENO, F_GETFL);
+    flag |= O_NONBLOCK;
+    fcntl(STDIN_FILENO, F_SETFL, flag);
+
+    flag = fcntl(cfd, F_GETFL);
+    flag |= O_NONBLOCK;
+    fcntl(cfd, F_SETFL, flag);
 
     while (1)
     {
