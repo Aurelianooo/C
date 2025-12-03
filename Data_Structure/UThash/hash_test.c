614121a050c5ad0b062f5070cc04c7cd221c6b26
Aureliano <54444048+Aurelianooo@users.noreply.github.com>
2025-12-03 09:06:58 +0800

    Add replace test for hash table functionality

--- Data_Structure/UThash/hash_test.c
+++ Data_Structure/UThash/hash_test.c
@@ -34,6 +34,24 @@ typedef struct
 } custom_t;
 #endif
 
+#ifdef REPLACE_TEST
+typedef struct hs_t
+{
+    int id;
+    int tag;
+    UT_hash_handle hh;
+} hs_t;
+
+static void pr(hs_t *_hdp)
+{
+    hs_t *el, *tmp, *hdp = _hdp;
+    HASH_ITER(hh, hdp, el, tmp)
+    {
+        printf("id=%d, tag=%d\n", el->id, el->tag);
+    }
+}
+#endif
+
 int main()
 {
 #ifdef BASIC_TEST
@@ -136,6 +154,58 @@ int main()
         HASH_DEL(customs, p);
         free(p);
     }
+#endif
+
+#ifdef REPLACE_TEST
+    hs_t *hs_head = NULL, *tmp, *replaced = NULL;
+
+    tmp = (hs_t *)malloc(sizeof(hs_t));
+    if (tmp == NULL)
+        exit(-1);
+    tmp->id = 10;
+    tmp->tag = 100;
+    HASH_REPLACE_INT(hs_head, id, tmp, replaced);
+    if (replaced == NULL)
+    {
+        printf("added %d %d\n", tmp->id, tmp->tag);
+    }
+    else
+    {
+        printf("ERROR, ended up replacing a value, replaced: %p\n", (void *)replaced);
+    }
+    pr(hs_head);
+
+    tmp = (hs_t *)malloc(sizeof(hs_t));
+    if (tmp == NULL)
+        exit(-1);
+    tmp->id = 11;
+    tmp->tag = 101;
+    HASH_REPLACE_INT(hs_head, id, tmp, replaced);
+    if (replaced == NULL)
+    {
+        printf("added %d %d\n", tmp->id, tmp->tag);
+    }
+    else
+    {
+        printf("ERROR, ended up replacing a value, replaced: %p\n", (void *)replaced);
+    }
+    pr(hs_head);
+
+    tmp = (hs_t *)malloc(sizeof(hs_t));
+    if (tmp == NULL)
+        exit(-1);
+    tmp->id = 11;
+    tmp->tag = 102;
+    HASH_REPLACE_INT(hs_head, id, tmp, replaced);
+    if (replaced == NULL)
+    {
+        printf("ERROR, exected to replace a value with key: %d\n", tmp->id);
+    }
+    else
+    {
+        printf("replaced %d that had tag %d with tag %d\n", tmp->id, replaced->tag, tmp->tag);
+    }
+    pr(hs_head);
 #endif
 
     return 0;
