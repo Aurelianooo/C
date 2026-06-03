fa0ec2f2182eefb38ed3eda46d9dcf103c374393
Aureliano <xxz__1995@sina.com>
2026-06-03 10:35:20 +0800

    Update client.c

--- Network_Program/Mychat/client.c
+++ Network_Program/Mychat/client.c
@@ -1,4 +1,4 @@
-#include "../wrap/wrap.h"
+#include "wrap.h"
 
 #define SERV_ADDR "127.0.0.1"
 #define SERV_PORT 8888
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     char buf[BUFSIZ];
-    int ret;
+    int i, ret;
 
     int cfd = Socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_addr;
@@ -24,16 +24,43 @@ int main(int argc, char *argv[])
     flag |= O_NONBLOCK;
     fcntl(cfd, F_SETFL, flag);
 
+    int efd = epoll_create(2);
+    struct epoll_event ev, events[2];
+    ev.events = EPOLLIN;
+    ev.data.fd = STDIN_FILENO;
+    epoll_ctl(efd, EPOLL_CTL_ADD, STDIN_FILENO, &ev);
+    ev.data.fd = cfd;
+    epoll_ctl(efd, EPOLL_CTL_ADD, cfd, &ev);
+
     while (1)
     {
-        ret = Read(STDIN_FILENO, buf, sizeof buf);
-        if (!strncmp(buf, "quit", 4))
-            break;
-        Write(cfd, buf, ret);
-        ret = Read(cfd, buf, sizeof buf);
-        Write(STDOUT_FILENO, buf, ret);
+        int nready = epoll_wait(efd, events, 2, -1);
+        for (i = 0; i < nready; i++)
+        {
+            if (events[i].data.fd == STDIN_FILENO)
+            {
+                ret = Read(STDIN_FILENO, buf, sizeof(buf));
+                if (ret > 0)
+                    Write(cfd, buf, ret);
+                if (!strncmp(buf, "quit", 4))
+                    goto exit;
+            }
+            else if (events[i].data.fd == cfd)
+            {
+                int ret = Read(cfd, buf, sizeof(buf));
+                if (ret > 0)
+                    Write(STDOUT_FILENO, buf, ret);
+                else if (ret == 0)
+                {
+                    printf("服务器已断开连接\n");
+                    goto exit;
+                }
+            }
+        }
     }
 
+exit:
     close(cfd);
+    close(efd);
     return 0;
 }
